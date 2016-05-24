d3b06995d83b250f1142cba803f354ff15fba0af
Peng Zhu <manlinzhu@gmail.com>
2016-05-24 22:25:44 +0800

    增加豆瓣api的调用地址和方法

--- imports/method/douban.js
+++ imports/method/douban.js
@@ -1,1 +1,20 @@
 import { HTTP } from 'meteor/http';
+
+if(Meteor.isServer) {
+    Meteor.methods({
+        fetchBookInfoFromDouban: (isbn) => {
+            //this.unblock();
+            try {
+                var uri = 'http://api.douban.com/v2/book/isbn/' + isbn;
+                var result = HTTP.call('GET', uri);
+                console.log(result.data);
+                return result;
+            }
+            catch(e) {
+                console.log('err');
+                console.log(e);
+                return false;
+            }
+        }
+    });
+}
--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 import { Books } from '../imports/api/books.js';
 
+import '../imports/method/douban.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
